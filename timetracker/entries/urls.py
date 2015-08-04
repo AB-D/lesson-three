26b5e270ccc32e3e808b654e3a1124f3ed3976c8
ari.dyball@gmail.com <aridyball@192-168-1-8.tpgi.com.au>
2015-08-04 20:41:23 +1000

    MDS homework week three
    Added urls for clients and projects
    Updated -each- template to use {% url %}
    Created views that list clients and projects
    Used template inheritance
    Created client summary page that lists projects and time entires using
    the pilfered table from ‘music_demo’
    Thank you all.

--- timetracker/entries/urls.py
+++ timetracker/entries/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import include, url
 from entries import views
 
 
+
 urlpatterns = [
     url(r'^entries/$', views.entries, name='entries'),
+    url(r'^projects/$', views.projects, name='projects'),
+    url(r'^clients/$', views.clients, name='clients'),
+    url(r'^summary/$', views.summary, name='summary'),
 ]
--- timetracker/entries/views.py
+++ timetracker/entries/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from .models import Entry
+from .models import Project
+from .models import Client
 
 
 def entries(request):
@@ -8,3 +10,21 @@ def entries(request):
     return render(request, 'entries.html', {
         'entry_list': entry_list,
     })
+
+def projects(request):
+	project_list = Project.objects.all()
+	return render(request, 'projects.html', {
+		'project_list' : project_list,
+	})
+
+def clients(request):
+	client_list = Client.objects.all()
+	return render(request, 'clients.html', {
+		'client_list' : client_list,
+	})
+
+def summary(request):
+	summary_all = Entry.objects.all() 
+	return render(request, 'summary.html', {
+		"summary_all": summary_all,
+		})
